8709f4aac210ad9dd7faad7e1cbbb621b10e726a
Maximilian Eschenbacher <maximilian@eschenbacher.email>
2022-02-02 17:21:20 +0100

    remove __init__

--- capirca/lib/cumulus.py
+++ capirca/lib/cumulus.py
@@ -8,13 +8,7 @@ _PLATFORM = 'cumulus'
 class Term(iptables.Term):
     _PLATFORM = 'cumulus'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
 
 class Cumulus(iptables.Iptables):
     _PLATFORM = 'cumulus'
     SUFFIX = '.ipt-cumulus'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
