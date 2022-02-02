8face7bbc73e7c02c2da0b8b7a0f89d6384ebd34
Maximilian Eschenbacher <maximilian@eschenbacher.email>
2022-02-02 19:17:27 +0100

    cumulus rules are loading

--- capirca/lib/cumulus.py
+++ capirca/lib/cumulus.py
@@ -1,14 +1,24 @@
 """Cumulus Linux iptables flavour."""
 
 from capirca.lib import iptables
+from string import Template
 
 _PLATFORM = 'cumulus'
 
 
 class Term(iptables.Term):
-    _PLATFORM = 'cumulus'
+  _PLATFORM = 'cumulus'
+  _PREJUMP_FORMAT = None
+  _TERM_FORMAT = None
+  _FILTER_TOP_FORMAT = Template('-A $filter')
+
+  def __init__(self, term, filter_name, trackstate, filter_action, af='inet',
+               verbose=True):
+    # do not track state, regardless of what is configured
+    super().__init__(term, filter_name, False, filter_action, af, verbose)
 
 
 class Cumulus(iptables.Iptables):
-    _PLATFORM = 'cumulus'
-    SUFFIX = '.ipt-cumulus'
+  _PLATFORM = 'cumulus'
+  SUFFIX = '.ipt-cumulus'
+  _TERM = Term
