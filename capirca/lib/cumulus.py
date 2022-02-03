e9b56220f1e3cbb1a44df2d9b9cf7b35657fc636
Maximilian Eschenbacher <maximilian@eschenbacher.email>
2022-02-03 19:56:52 +0100

    cumulus: prepend [ip6?tables] to output

--- capirca/lib/cumulus.py
+++ capirca/lib/cumulus.py
@@ -22,3 +22,17 @@ class Cumulus(iptables.Iptables):
   _PLATFORM = 'cumulus'
   SUFFIX = '.ipt-cumulus'
   _TERM = Term
+
+  def _TranslatePolicy(self, pol, exp_info):
+    ret = super()._TranslatePolicy(pol, exp_info)
+    if 'inet6' in self.filter_options:
+      self.address_family = 'inet6'
+    else:
+      self.address_family = 'inet'
+    return ret
+
+  def __str__(self):
+    if self.address_family == 'inet6':
+      return '[ip6tables]\n%s' % super().__str__()
+    else:
+      return '[iptables]\n%s' % super().__str__()
