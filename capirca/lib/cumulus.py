ebe7baac1516aff9867ca65cf7098ec4ffcb4892
Maximilian Eschenbacher <maximilian@eschenbacher.email>
2022-02-06 11:25:15 +0100

    cumulus: always generate both address families
    ... in the format which cumulus linux expects it:
    
    [ip6tables]
    ...
    [iptables]
    ...

--- capirca/lib/cumulus.py
+++ capirca/lib/cumulus.py
@@ -2,6 +2,7 @@
 
 from capirca.lib import iptables
 from string import Template
+import io
 
 _PLATFORM = 'cumulus'
 
@@ -12,27 +13,48 @@ class Term(iptables.Term):
   _TERM_FORMAT = None
   _FILTER_TOP_FORMAT = Template('-A $filter')
 
-  def __init__(self, term, filter_name, trackstate, filter_action, af='inet',
-               verbose=True):
-    # do not track state, regardless of what is configured
-    super().__init__(term, filter_name, False, filter_action, af, verbose)
-
 
 class Cumulus(iptables.Iptables):
   _PLATFORM = 'cumulus'
   SUFFIX = '.ipt-cumulus'
   _TERM = Term
 
-  def _TranslatePolicy(self, pol, exp_info):
-    ret = super()._TranslatePolicy(pol, exp_info)
-    if 'inet6' in self.filter_options:
-      self.address_family = 'inet6'
-    else:
-      self.address_family = 'inet'
-    return ret
+  def __init__(self, pol, exp_info):
+    # call once
+    super().__init__(pol, exp_info)
+    self.pol = pol
+    self.exp_info = exp_info
+
+
+  def _add_af_to_filter(self, af='inet6'):
+    """
+    af: inet6 or inet
+    """
+    for header, terms in self.pol.filters:
+      if self._PLATFORM not in header.platforms:
+        continue
+      filter_options = header.FilterOptions(self._PLATFORM)
+      if af not in filter_options:
+        filter_options.append(af)
+
+  def _remove_af_from_filter(self, af='inet6'):
+    """
+    af: inet6 or inet
+    """
+    for header, terms in self.pol.filters:
+      if self._PLATFORM not in header.platforms:
+        continue
+      filter_options = header.FilterOptions(self._PLATFORM)
+      if af in filter_options:
+        filter_options.remove(af)
 
   def __str__(self):
-    if self.address_family == 'inet6':
-      return '[ip6tables]\n%s' % super().__str__()
-    else:
-      return '[iptables]\n%s' % super().__str__()
+    ret = io.StringIO()
+    for af in ['inet6', 'inet']:
+      self._add_af_to_filter(af=af)
+      # reset rules
+      self.iptables_policies = []
+      self._TranslatePolicy(self.pol, self.exp_info)
+      ret.write('[ip%stables]\n%s' % ('6' if af == 'inet6' else '', super().__str__()))
+      self._remove_af_from_filter(af=af)
+    return ret.getvalue()
